83b7de9be80f6c6fe86e840244d09cca2650026e
weazy <weazy.exe@gmail.com>
2019-12-23 17:14:50 +0300

    Add project files.

---
+++ affdex-app/MainForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace AffdexApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImagePicBox = new System.Windows.Forms.PictureBox();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.ProcessButton = new System.Windows.Forms.Button();
+            this.LogLabel = new System.Windows.Forms.Label();
+            this.LogListView = new System.Windows.Forms.ListBox();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImagePicBox
+            // 
+            this.ImagePicBox.Location = new System.Drawing.Point(250, 12);
+            this.ImagePicBox.Name = "ImagePicBox";
+            this.ImagePicBox.Size = new System.Drawing.Size(437, 443);
+            this.ImagePicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ImagePicBox.TabIndex = 0;
+            this.ImagePicBox.TabStop = false;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(24, 12);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(89, 41);
+            this.LoadButton.TabIndex = 1;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // ProcessButton
+            // 
+            this.ProcessButton.Location = new System.Drawing.Point(132, 12);
+            this.ProcessButton.Name = "ProcessButton";
+            this.ProcessButton.Size = new System.Drawing.Size(93, 41);
+            this.ProcessButton.TabIndex = 2;
+            this.ProcessButton.Text = "Process";
+            this.ProcessButton.UseVisualStyleBackColor = true;
+            this.ProcessButton.Click += new System.EventHandler(this.ProcessButton_Click);
+            // 
+            // LogLabel
+            // 
+            this.LogLabel.AutoSize = true;
+            this.LogLabel.Location = new System.Drawing.Point(21, 73);
+            this.LogLabel.Name = "LogLabel";
+            this.LogLabel.Size = new System.Drawing.Size(25, 13);
+            this.LogLabel.TabIndex = 4;
+            this.LogLabel.Text = "Log";
+            // 
+            // LogListView
+            // 
+            this.LogListView.FormattingEnabled = true;
+            this.LogListView.Location = new System.Drawing.Point(24, 90);
+            this.LogListView.Name = "LogListView";
+            this.LogListView.Size = new System.Drawing.Size(201, 95);
+            this.LogListView.TabIndex = 5;
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(24, 241);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(0, 13);
+            this.ResultLabel.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 467);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.LogListView);
+            this.Controls.Add(this.LogLabel);
+            this.Controls.Add(this.ProcessButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.ImagePicBox);
+            this.Name = "MainForm";
+            this.Text = "Affdex App - Load an image";
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePicBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImagePicBox;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button ProcessButton;
+        private System.Windows.Forms.Label LogLabel;
+        private System.Windows.Forms.ListBox LogListView;
+        private System.Windows.Forms.Label ResultLabel;
+    }
+}
+
---
+++ affdex-app/MainForm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+using Affdex;
+
+namespace AffdexApp
+{
+    public partial class MainForm : Form, ImageListener
+    {
+        private PhotoDetector detector;
+        private Image image;
+        private Graphics graphics;
+        private Stopwatch stopwatch;
+        private const string AffdexApp = "Affdex App - ";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            detector = new PhotoDetector(2, FaceDetectorMode.SMALL_FACES);
+
+            var classifierPath = @"D:\dev\AffdexSDK\data";
+            detector.setClassifierPath(classifierPath);
+
+            detector.setDetectAllExpressions(true);
+            detector.setDetectAllEmotions(true);
+            detector.setDetectAllEmojis(true);
+            detector.setDetectAllAppearances(true);
+
+            detector.setImageListener(this);
+
+            stopwatch = new Stopwatch();
+        }
+
+        public void onImageResults(Dictionary<int, Face> faces, Frame frame)
+        {
+            stopwatch.Stop();
+            var face = faces.First().Value;
+
+            SetResultLabel(face);
+            DrawFeaturePoints(face.FeaturePoints);
+
+            Text = AffdexApp + "Done";
+            stopwatch.Reset();
+        }
+
+        public void onImageCapture(Frame frame)
+        {
+            Text = AffdexApp + "Captured. Find faces...";
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Title = "Open image";
+                ofd.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    image = Image.FromFile(ofd.FileName);
+                    ImagePicBox.Image = image;
+                    graphics = ImagePicBox.CreateGraphics();
+                    Text = AffdexApp + "Ok, now find some emotions";
+                }
+            }
+        }
+
+        private void ProcessButton_Click(object sender, EventArgs e)
+        {
+            var frame = ImageToFrame(image);
+            Log("Frame created");
+            Text = AffdexApp + "Capturing...";
+
+            stopwatch.Start();
+            detector.start();
+            Log("Detector started. Processing...");
+
+            try
+            {
+                detector.process(frame);
+                Log("Done");
+            }
+            catch (AffdexException ex)
+            {
+                ResultLabel.Text = $"Affdex error: {ex.Message}";
+                Log("Processing ends with error");
+            }
+            
+            
+            detector.stop();
+        }
+
+        private Frame ImageToFrame(Image image)
+        {
+
+            Bitmap bitmap = (Bitmap) image;
+
+            // Lock the bitmap's bits.
+            Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            BitmapData bmpData = bitmap.LockBits(rect, ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+            // Get the address of the first line.
+            IntPtr ptr = bmpData.Scan0;
+
+            // Declare an array to hold the bytes of the bitmap. 
+            int numBytes = bitmap.Width * bitmap.Height * 3;
+            byte[] rgbValues = new byte[numBytes];
+
+            int data_x = 0;
+            int ptr_x = 0;
+            int row_bytes = bitmap.Width * 3;      //3 bytes per pixel
+
+            // The bitmap requires bitmap data to be byte aligned.
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                Marshal.Copy(ptr + ptr_x, rgbValues, data_x, row_bytes);//(pixels, data_x, ptr + ptr_x, row_bytes);
+                data_x += row_bytes;
+                ptr_x += bmpData.Stride;
+            }
+
+            bitmap.UnlockBits(bmpData);
+
+            return new Frame(bitmap.Width, bitmap.Height, rgbValues, Affdex.Frame.COLOR_FORMAT.RGB);
+        }
+
+        private void Log(string message)
+        {
+            LogListView.Items.Add(message);
+        }
+
+        private void SetResultLabel(Face face)
+        {
+            var result = new StringBuilder();
+
+            result.Append($"Total processing time: {stopwatch.Elapsed.TotalMilliseconds / 1000}s\n\n");
+
+            result.Append("Emotions:\n");
+            result.Append($"Anger: {face.Emotions.Anger}\n");
+            result.Append($"Disgust: {face.Emotions.Disgust}\n");
+            result.Append($"Fear: {face.Emotions.Fear}\n");
+            result.Append($"Sadness: {face.Emotions.Sadness}\n");
+            result.Append($"Joy: {face.Emotions.Joy}\n");
+            result.Append($"Surprise: {face.Emotions.Surprise}\n");
+            result.Append($"Contempt: {face.Emotions.Contempt}\n");
+            result.Append($"Engagement: {face.Emotions.Engagement}\n");
+            result.Append($"Valence: {face.Emotions.Valence}\n");
+            result.Append($"Fear: {face.Emotions.Fear}\n\n");
+
+            ResultLabel.Text = result.ToString();
+        }
+
+        private void DrawFeaturePoints(FeaturePoint[] featurePoints)
+        {
+            var pen = new Pen(Color.Red, 2);
+
+            var ratioX = ImagePicBox.Width / (float)image.Width;
+            var ratioY = ImagePicBox.Height / (float)image.Height;
+
+            foreach (var featurePoint in featurePoints)
+            {
+                graphics.DrawEllipse(pen, featurePoint.X * ratioX, featurePoint.Y * ratioY, 2, 2);
+            }
+        }
+    }
+}
